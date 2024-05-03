e88ef4403ad706ee939705dca6721753b10f93df
Cameron Bristol <cbristol28@yahoo.com>
2024-05-03 13:33:07 -0500

    Create AuthenticatedNav component

---
+++ app/components/authenticated-nav/authenticated-nav.tsx
@@ -0,0 +1,23 @@
+import { truncateAddress } from "@/app/utils/truncate-address";
+import { useMetaMask } from "metamask-react";
+import { PropsWithChildren } from "react";
+
+export function AuthenticatedNav({ children }: PropsWithChildren) {
+  const { account } = useMetaMask();
+
+  if (!account)
+    throw new Error("User not logged in! Please log in to see this page");
+
+  return (
+    <div className="flex flex-col h-[100svh] overflow-y-clip">
+      <nav className="flex justify-between w-full px-4 py-2 bg-slate-500">
+        <p>BitBizaar</p>
+        <p className="font-md">
+          Logged In:{" "}
+          <span className="font-bold">{truncateAddress(account)}</span>!
+        </p>
+      </nav>
+      {children}
+    </div>
+  );
+}
---
+++ app/utils/truncate-address.ts
@@ -0,0 +1,8 @@
+export function truncateAddress(address: string) {
+  if (address.length < 10) return address;
+
+  return `${address.substring(0, 4)}...${address.substring(
+    address.length - 4,
+    address.length
+  )}`;
+}
